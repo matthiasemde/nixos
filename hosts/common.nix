e6f45a6015fe170da65f5d82aae9a09d6584a221
Matthias Emde <matthias@emdemail.de>
2026-05-29 23:08:30 +0200

    refactor: move common host config to separate file

---
+++ hosts/common.nix
@@ -0,0 +1,65 @@
+{ pkgs, ... }:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+
+  console.keyMap = "de";
+
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    optimise.automatic = true;
+  };
+
+  users.users.matthias = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "docker"
+      "libvirtd"
+      "kvm"
+      "ykusers"
+    ];
+    linger = true;
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:matthiasemde/nixos";
+    dates = "*-*-* 05:00:00 Europe/Berlin";
+  };
+}
--- hosts/mahler/configuration.nix
+++ hosts/mahler/configuration.nix
@@ -1,101 +1,26 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 {
   config,
   lib,
   pkgs,
   ...
 }:
-
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "mahler"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.utf8";
-    LC_IDENTIFICATION = "de_DE.utf8";
-    LC_MEASUREMENT = "de_DE.utf8";
-    LC_MONETARY = "de_DE.utf8";
-    LC_NAME = "de_DE.utf8";
-    LC_NUMERIC = "de_DE.utf8";
-    LC_PAPER = "de_DE.utf8";
-    LC_TELEPHONE = "de_DE.utf8";
-    LC_TIME = "de_DE.utf8";
-  };
+  networking.hostName = "mahler";
 
   console = {
     font = "Lat2-Terminus16";
     keyMap = lib.mkForce "de";
-    useXkbConfig = true; # use xkb.options in tty.
+    useXkbConfig = true;
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # Clean up nix store and remove old generations automatically
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-  nix.optimise.automatic = true; # Optimize the Nix store periodically
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
   security.sudo.wheelNeedsPassword = false;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.matthias = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-    ]; # Enable ‘sudo’ for the user.
-    linger = true; # make sure services like docker persist after logout
-  };
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    git
     bat
     btop
     pciutils
@@ -104,34 +29,17 @@
     age-plugin-yubikey
   ];
 
-  services.pcscd = {
-    enable = true;
-  };
-
+  services.pcscd.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services = {
-    openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
-      };
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
     };
   };
 
-  # Enable Prometheus Node Exporter for system metrics
   services.prometheus.exporters.node = {
     enable = true;
     port = 9100;
@@ -142,31 +50,16 @@
       "loadavg"
       "meminfo"
       "netdev"
-      # "stat"
-      # "time"
-      # "vmstat"
-      # "logind"
-      # "interrupts"
-      # "ksmd"
     ];
   };
 
-  # Automatic deployment: pull origin/main every day at 05:00 and rebuild.
-  system.autoUpgrade = {
-    enable = true;
-    flake = "github:matthiasemde/nixos";
-    dates = "*-*-* 05:00:00 Europe/Berlin";
-  };
-
-  # Open ports in the firewall.
   networking = {
     resolvconf.enable = false;
     firewall = {
       enable = true;
       allowedTCPPorts = [
         53 # Allow TCP DNS
-        9100 # Prometheus Node Exporter
-        9323 # Prometheus Docker metrics
+        9100 # Node exporter metrics
       ];
       allowedUDPPorts = [ 53 ]; # Allow UDP DNS
     };
@@ -176,27 +69,5 @@
     "resolv.conf".text = "nameserver 192.168.178.1\n";
   };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
